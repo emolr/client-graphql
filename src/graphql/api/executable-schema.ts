bfbf7acbfcfdcc320ff4349da608061e3c8dab04
Emil Rune Møller <emilrmoeller@gmail.com>
2021-08-23 13:15:11 +0200

    build succeeding, has to disable graphql-tag-schema

--- src/graphql/api/executable-schema.ts
+++ src/graphql/api/executable-schema.ts
@@ -1,6 +1,20 @@
 import { makeExecutableSchema } from '@graphql-tools/schema';
 import { resolvers } from './resolvers';
-import { schema as typeDefs } from './generated/schema';
+// import { schema as typeDefs } from './generated/schema';
+
+const typeDefs = `
+  type Query {
+    pokemon(id: Int!): Pokemon!
+    pokemons(offset: Int, first: Int): [Pokemon!]!
+  }
+
+  type Pokemon {
+    id: Int!
+    name: String!
+    image: String!
+    type: String!
+  }
+`;
 
 export const schema = makeExecutableSchema({
   typeDefs,
